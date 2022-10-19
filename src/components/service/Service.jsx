121ff8e1d178b7b149ec00c2b77dbe2ed11261eb
Joel Bascombe <joelbascombe13@gmail.com>
2022-10-19 13:26:35 -0400

    Add components

---
+++ src/components/service/Service.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./service.css";
+
+const Service = () => {
+  return <div>Service</div>;
+};
+
+export default Service;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import { Provider } from "react-redux";
+// import { Router } from "react-router-dom";
+// import history from "./history";
+// import store from "./store";
+import App from "./App";
+import "./style.css";
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+{
+  /* <Provider store={store}>
+<Router history={history}>
+  <App />
+</Router>
+</Provider>, */
+}
